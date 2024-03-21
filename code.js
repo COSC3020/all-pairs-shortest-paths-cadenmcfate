171ebaa04f8c25ab0e534228da541207f82dc122
Caden McFate <cadenmcfate@gmail.com>
2024-03-21 11:35:15 -0600

    implimented allPairsShortestPaths & tests

--- code.js
+++ code.js
@@ -1,3 +1,23 @@
 function allPairsShortestPaths(graph) {
-    return [];
+    let dist = [];
+    for (let i = 0; i < graph[0].length; i++) {
+        dist[i] = [];
+        for (let j = 0; j < graph[0].length; j++) {
+            dist[i][j] = Infinity;
+        }
+        dist[i][i] = 0;
+    }
+    for (let i = 0; i < graph[1].length; i++) {
+        let edge = graph[1][i];
+        let [u,v,weight] = edge;
+        dist[u][v] = weight;
+    }
+    for (let k = 0; k < graph[0].length; k++) {
+        for (let i = 0; i < graph[0].length; i++) {
+            for (let j = 0; j < graph[0].length; j++) {
+                dist[i][j] = Math.min(dist[i][j],dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+    return dist;
 }
